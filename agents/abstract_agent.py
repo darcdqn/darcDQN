f2d3d661e2a24a1e52f23014cb3809d863dbc777
Pontus Eriksson <pontus_eriksson@live.com>
2019-02-13 13:35:19 +0100

    Abstract class for Agents

---
+++ agents/abstract_agent.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractAgent(ABC):
+
+    """
+    Description:
+        An agent is a, possibly but not necessarily trainable, policy
+        for which action to take given the current state of the system.
+    """
+
+    def __init__(self, n_inputs, n_outputs):
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
+
+    @abstractmethod
+    def get_action(self, state):
+        """Return which action to take given the current state."""
+        pass
+
+
