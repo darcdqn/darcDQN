f3f20122b538afa76a97a29a0834d590ef126b87
love <lovewestlundgotby@gmail.com>
2019-02-14 14:05:37 +0100

    Environment ID should be capitalized

--- darcDQN/__init__.py
+++ darcDQN/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-        id='berry-v0',
+        id='Berry-v0',
         entry_point='envs.berry:BerryEnv',
         max_episode_steps=200,
         reward_threshold=190.0,
