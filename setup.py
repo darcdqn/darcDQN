8dc931153e172b22891ff9d99d35ab39b029c490
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2019-02-15 16:46:35 +0100

    Added tensorflow as dependency. Tensorflow is only required for DQNAgent at this time but will be needed in the future.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name='darcDQN',
       version='0.0.1',
       packages=find_packages(),
-      install_requires=['gym[atari,classic_control]'],
+      install_requires=['gym[atari,classic_control]', 'tensorflow'],
 )
 
