293654a328cddb16c9db716880b682f5a0b635eb
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2019-02-15 11:22:09 +0100

    Changed berry env id

--- darcDQN/__init__.py
+++ darcDQN/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
         id='Berry-v0',
-        entry_point='envs.berry:BerryEnv',
+        entry_point='darcDQN.envs.berry:BerryEnv',
         max_episode_steps=200,
         reward_threshold=190.0,
 )
