c4065709d1543342f525882cdfefd96880c9b51c
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2019-02-19 16:08:12 +0100

    Improvements to how things are printed when training.

--- run.py
+++ run.py
@@ -2,15 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import numpy as np
 
 from darcDQN.utils.cmd_util import common_arg_parser
 from darcDQN import agents
 from darcDQN import utils
-from gym import error, logger, envs
+from gym import logger, envs
+from gym.envs.registration import registry
+
+
+def run_episode(env, agent, mode, num_timesteps):
+    obs = env.reset()
+    episode_reward = 0.0
+    #  TODO: figure the render thing out #
+    if mode is not None:
+        env.render(mode)
+    for timestep in range(num_timesteps):
+        done = False
+
+        action = agent.get_action(obs)
+
+        new_obs, reward, done, info_dict = env.step(action)
+        episode_reward += reward
+
+        agent.observe(obs, new_obs, action, reward, done)
+        obs = new_obs
+
+        if mode is not None:
+            env.render(mode)
+
+        if done:
+            break
+    return episode_reward, timestep
 
 
 def train(args):
     env = envs.make(args.env)
+
+    load_path = args.load_path
+    save_path = args.save_path
 
     n_inputs = 0
     for i in range(len(env.observation_space.shape)):
@@ -23,43 +53,69 @@ def train(args):
 
     agent = agents.make(args.agent, **kwargs)
 
+    if load_path is not None:
+        agent.load_agent(load_path)
+
     logger.info('Training agent {} in env {}'.format(args.agent, args.env))
 
-    mode = args.mode
-
-    epoch = 0
-    if args.num_epochs == 0:
-        num_epochs = -1
+    if args.mode.lower() == 'none':
+        mode = None
     else:
-        num_epochs = int(args.num_epochs)
-    num_timesteps = int(args.num_timesteps)
+        mode = args.mode
 
-    epoch_rewards = []
-    while epoch != num_epochs:
-        epoch_rewards.append(0)
-        obs = env.reset()
-        #  TODO: figure the render thing out #
-        env.render(mode)
-        logger.info('Epoch {} of {}'.format(epoch, num_epochs))
-        for t in range(num_timesteps):
-            done = False
+    spec = registry.env_specs[args.env]
+    trials = spec.trials
+    reward_threshold = spec.reward_threshold
+    num_timesteps = args.num_timesteps or spec.max_episode_steps
 
-            action = agent.get_action(obs)
+    episode = 1
+    num_episodes = int(args.num_episodes)
 
-            new_obs, reward, done, info_dict = env.step(action)
-            epoch_rewards[-1] += reward
+    trial_rewards = np.zeros(trials)
+    max_average_reward = 0.0
+    total_timesteps = 0
+    is_solved = False
 
-            agent.observe(obs, new_obs, action, reward, done)
-            obs = new_obs
+    while not is_solved and episode != num_episodes:
+        episode_index = (episode - 1) % trials
+        trial_rewards[episode_index] = 0.0
 
-            env.render(mode)
+        episode_reward, episode_steps = run_episode(env,
+                                                    agent,
+                                                    mode,
+                                                    num_timesteps)
 
-            if done:
-                break
+        trial_rewards[episode_index] += episode_reward
+        total_timesteps += episode_steps
 
-        logger.info(('Epoch {} finished after {} timesteps with a total score '
-                     + 'of {}').format(epoch, t, epoch_rewards[-1]))
-        epoch += 1
+        trial_average_reward = np.mean(trial_rewards)
+        if episode > trials + 1 and trial_average_reward > reward_threshold:
+            is_solved = True
+
+        if episode % trials == 0:
+            if trial_average_reward > max_average_reward:
+                logger.info('Maximum average reward: {:.1f} -> {:.1f}'
+                            .format(max_average_reward, trial_average_reward))
+                max_average_reward = trial_average_reward
+                if save_path is not None:
+                    agent.save_agent(save_path)
+                    logger.info('Agent saved')
+
+            logger.info(('Episode {}'
+                         + ' : {} timesteps'
+                         + ' : {}-trial average reward {:.1f}')
+                        .format(episode,
+                                total_timesteps,
+                                trials,
+                                trial_average_reward))
+
+        episode += 1
+        if is_solved:
+            if save_path is not None:
+                agent.save_agent(save_path)
+            logger.info(('Agent finished with average score {:.1f}'
+                        .format(trial_average_reward)))
+            break
     return agent, env
 
 
@@ -92,10 +148,7 @@ def main(args):
     logger.set_level(logger.INFO)
 
     """ Train """
-    if args.num_epochs is not None and args.num_timesteps is not None:
-        trained_agent, env = train(args)
-    else:
-        logger.info('num_epochs or num_timesteps not provided, not training')
+    trained_agent, env = train(args)
 
     """ Play """
     if args.play:
